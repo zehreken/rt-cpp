66088cd5bb69d71d4acadd95d0b58c61d6f36b20
zehreken <zehreken@gmail.com>
2019-10-05 22:43:37 +0200

    Add camera, update main

---
+++ rt_1/rt_1/hitable.h
@@ -0,0 +1,19 @@
+#ifndef hitable_h
+#define hitable_h
+
+#include "ray.h"
+
+struct hit_record
+{
+	float t;
+	vec3 p;
+	vec3 normal;
+};
+
+class hitable
+{
+public:
+	virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+#endif /* hitable_h */
--- rt_1/rt_1/main.cpp
+++ rt_1/rt_1/main.cpp
@@ -1,50 +1,24 @@
 #include <iostream>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#include "vec3.h"
-#include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
+#include "camera.h"
+#include "float.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r)
+vec3 color(const ray& r, hitable *world)
 {
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
-	
-	if (discriminant < 0)
+	hit_record rec;
+	if (world->hit(r, 0.0, MAXFLOAT, rec))
 	{
-		return -1;
+		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 	else
 	{
-		return (-b - sqrt(discriminant)) / (2.0 * a);
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
-}
-
-vec3 color(const ray& r)
-{
-//	if (hit_sphere(vec3(0, 0, 1), 0.5, r))
-//	{
-//		return vec3(1, 0, 0); // color red
-//	}
-//	if (hit_sphere(vec3(1, 0, -2), 0.5, r))
-//	{
-//		return vec3(0, 0, 1); // color blue
-//	}
-//	if (hit_sphere(vec3(-2, 0, -3), 0.5, r))
-//	{
-//		return vec3(0, 0, 0); // color green
-//	}
-	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-	if (t > 0)
-	{
-		vec3 N = unit_vector(r.point_at(t) - vec3(0, 0, -1));
-		return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-	}
-	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main(int argc, const char * argv[])
@@ -58,6 +32,12 @@ int main(int argc, const char * argv[])
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
 	
+	hitable *list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hitable *world = new hitable_list(list, 2);
+	camera cam;
+	
 	unsigned char imgData[nx * ny * 3];
 	int index = 0;
 //	std::cout<< "P3\n" << nx << " " << ny << "\n255\n";
@@ -65,12 +45,11 @@ int main(int argc, const char * argv[])
 	{
 		for (int i = 0; i < nx; i++)
 		{
-//			vec3 color(float(i) / float(nx), float(j) / float(ny), 0.2);
-			
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 col = color(r);
+			ray r = cam.get_ray(u, v);
+			vec3 p = r.point_at(2.0);
+			vec3 col = color(r, world);
 			
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
